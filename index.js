061d9b7f8a596e4104dea116cd8e4d14efd8c2fa
maniplek <maniplek81@gmail.com>
2022-07-04 15:57:35 +0200

    setting up backend stacture with database  model

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://memories:iZB11gtkjJIXGge9@cluster0.ds2ljdr.mongodb.net/?retryWrites=true&w=majority";
+  const PORT = process.env.PORT || 4000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true}) 
+    .then(()=> app.listen(PORT, () => console.log(`Sever is running on http://localhost:${PORT}`)))
+    .catch((error)=> console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
