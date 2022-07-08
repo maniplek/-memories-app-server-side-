99e5ee20fa050bd974d9253eca20bea7b8dff17f
maniplek <maniplek81@gmail.com>
2022-07-08 10:10:13 +0200

    connected to client

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://memories:iZB11gtkjJIXGge9@cluster0.ds2ljdr.mongodb.net/?retryWrites=true&w=majority";
